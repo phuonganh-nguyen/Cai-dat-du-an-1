b9a1cedcd3f84aafa87f2cdf0875dbd6cf50f345
Anh Huy <onlinetran13@gmail.com>
2023-10-06 19:59:27 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const contactsRouter = require("./app/routes/contact.route");
 const cors = require("cors");
 
 const app= express();
 
+app.use("/api/contacts", contactsRouter);
 app.use(cors());
 app.use(express.json());
 
